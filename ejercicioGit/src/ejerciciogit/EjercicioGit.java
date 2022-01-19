4cdc67b0b045be6b23386bf4461ee91598a4d433
josu3 <josu3@JOSUEPC>
2022-01-19 09:35:21 +0100

    Segundo commit

---
+++ ejercicioGit/src/ejerciciogit/EjercicioGit.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciogit;
+
+/**
+ *
+ * @author josu3
+ */
+public class EjercicioGit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Hola");
+        System.out.println("HOy es 19 de enero");
+    }
+    
+}
